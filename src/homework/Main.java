198eac2184a18ef73c1ea204d8f21b348638f582
anaSuzic <suzicka01@gmail.com>
2026-01-22 23:52:32 -0500

    final

--- src/homework/Main.java
+++ src/homework/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
 
         String[] word = getStrings();
         Random randomNum = new Random();
